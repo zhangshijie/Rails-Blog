1cf91b8c659abb767ab64f9fad71742d2fb0093e
 zhangshijie <cjzhang88@gmail.com>
2016-10-28 01:03:49 +0800

    删除操作

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -20,7 +20,7 @@ class ArticlesController < ApplicationController
 	def index
      @articles = Article.all
 	end
-	
+
 	def edit
 		@article = Article.find(params[:id])
 	end
@@ -34,6 +34,15 @@ class ArticlesController < ApplicationController
        render 'edit'
 		end
 	end
+
+  def destroy
+  	@article = Article.find(params[:id])
+  	@article.destroy
+
+  	redirect_to articles_path
+  end
+
+
 	private
 
 	def article_params
